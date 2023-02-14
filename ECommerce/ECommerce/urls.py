78840e45bdecedd81b723c15dcc232563a4c630a
Lê Chí Luân <chiluan6601@gmail.com>
2023-02-14 18:53:04 +0700

    index.html templates

--- ECommerce/ECommerce/urls.py
+++ ECommerce/ECommerce/urls.py
@@ -1,4 +1,4 @@
-"""ECommerce URL Configuration
+"""ecommerce URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('authentication.urls'))
+    path('', include('mysite.urls')),
 ]
