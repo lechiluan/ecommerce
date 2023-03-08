40fddfba5b58b44d7a865cb7e63114749c1f3e60
Lê Chí Luân <chiluan6601@gmail.com>
2023-03-08 21:34:02 +0700

    delete code because error

--- ECommerce/ECommerce/emails.py
+++
@@ -1,6 +0,0 @@
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'lclshop.dev@gmail.com'
-EMAIL_FROM = 'LCL Shop <lclshop.dev@gmail.com>'
-EMAIL_HOST_PASSWORD = 'wpjvecsxcwflfqsf'
-EMAIL_PORT = 587
