73c78ad8e4222e0f1531956351de2afab3e26fda
Lê Chí Luân <chiluan6601@gmail.com>
2023-04-22 16:43:27 +0700

    update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -46,8 +46,8 @@ SECRET_KEY = os.environ['SECRET_KEY']
 # SECURE_SSL_REDIRECT = True  # for https only, it uses to redirect http to https
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+# DEBUG = True
+DEBUG = False
 
 # ALLOWED_HOSTS is a list of strings representing the host/domain names that this Django site can server
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'lclshop.herokuapp.com', 'lclshop.up.railway.app']
@@ -78,7 +78,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',  # for authentication
     'django.contrib.messages.middleware.MessageMiddleware',  # for messages middleware
     'django.middleware.clickjacking.XFrameOptionsMiddleware',  # for clickjacking
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',  # for whitenoise
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # for whitenoise
 ]
 ROOT_URLCONF = 'ecommerce.urls'
 
@@ -157,12 +157,11 @@ STATICFILES_DIRS = [(BASE_DIR / 'static'),
                     (BASE_DIR / 'dashboard/static'),
                     (BASE_DIR / 'customer/static')]  # use to find static files
 STATIC_ROOT = BASE_DIR / 'staticfiles'  # use to collect static files use when use public hosting
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
-# MEDIA_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
+DEFAULT_FILE_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Connect to bootstrap4 for crispy forms
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
