c5d413b6195f81c5f4f98bdf1250c9dcf2068934
Le Chi Luan <chiluan6601@gmail.com>
2026-06-22 21:36:49 +0700

    fix: add --clear option to collectstatic command for better asset management

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@ set -o errexit
 
 python -m pip install --upgrade pip setuptools wheel
 pip install -r requirements.txt
-python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input --clear
 python manage.py migrate
